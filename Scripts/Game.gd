1d0c1101d882f43dd494ca9f38b5c80376e58f09
SKYNETTEDDRAGON <skynetteddragon@gmail.com>
2025-11-08 07:12:00 +0530

    game.gd made

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,11 +1,17 @@
 extends Node
 
+@export var grid_pos: Vector2i
+var has_mine: bool = false
+var revealed: bool = false
+var mine_count: int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func reveal():
+	if revealed:
+		return
+	revealed = true
+	if has_mine:
+		$tile.modulate = Color(1,0.2,0.2)
+	else:
+		$tile.modulate = Color(0.8,0.9,1.0)
+		$mine_count.text = str(mine_count) if mine_count >0 else ""
+		 
