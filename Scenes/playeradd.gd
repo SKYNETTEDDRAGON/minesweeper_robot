75c6c491a70fa643430d5409d4eebd8d11d1145e
SKYNETTEDDRAGON <skynetteddragon@gmail.com>
2025-11-08 10:24:40 +0530

    update to playeradd.gd
    polayer spawns but doesnt move

--- Scenes/playeradd.gd
+++ Scenes/playeradd.gd
@@ -7,8 +7,5 @@ func _ready():
 	# Instance the player
 	var player = player_scene.instantiate()
 	
-	# Set position to spawn point
-	player.global_transform.origin = spawn_point.global_transform.origin
-
 	# Add player to scene
 	add_child(player)
